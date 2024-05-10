2f547d82ab12365d2b2a6f61ced3e14f817be654
Mary Hudner <Mhudner@yahoo.com>
2024-05-10 16:19:22 -0600

    new commit

---
+++ BooksREST/src/main/webapp/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(e){
+	
+	
+	loadBooks();
+	//configureLookUpByIdForm look up form
+	
+	
+});
+
+let loadBooks = function() {
+	//xhr for all books 
+	
+	//display in table
+	
+	
+}
